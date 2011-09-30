5b04f5db051e37ccd61b7a47fc9a8517c186ed3e
Boris Bera <bboris@rsoft.ca>
2011-09-30 18:25:22 -0400

    Added messageManager and init method to engine

--- src/name/bobnet/android/rl/core/GameEngine.java
+++ src/name/bobnet/android/rl/core/GameEngine.java
@@ -5,16 +5,36 @@
  */
 package name.bobnet.android.rl.core;
 
+import name.bobnet.android.rl.core.message.MessageManager;
+
 public class GameEngine {
 	
 	// engine singleton
 	private static GameEngine engine;
 	
+	// variables
+	private MessageManager messageManager;
+	
+	private GameEngine() {
+		// create a message manager
+		messageManager = new MessageManager();
+	}
+	
+	/**
+	 * Starts the game
+	 */
+	public void init() {
+		// TODO: Init engine
+	}
+	
+	/**
+	 * @return the game engine singleton
+	 */
 	public static GameEngine getEngine() {
 		if (engine == null)
 			engine = new GameEngine();
 		
 		return engine;
 	}
-
+	
 }
