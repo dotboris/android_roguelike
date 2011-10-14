009d178084fd7f887c5ea9f0b94f83a89eca72a6
Boris Bera <bboris@rsoft.ca>
2011-10-14 11:47:35 -0400

    Added a floor tile type

---
+++ src/name/bobnet/android/rl/core/ents/tiles/Floor.java
@@ -0,0 +1,34 @@
+/*
+ * Class Name:			Floor.java
+ * Class Purpose:		A floor
+ * Created by:			boris on 2011-10-14
+ */
+package name.bobnet.android.rl.core.ents.tiles;
+
+import name.bobnet.android.rl.core.message.Message;
+
+public class Floor extends TileType {
+
+	public Floor(TileStyle style) {
+		super(style, true);
+	}
+
+	/* (non-Javadoc)
+	 * @see name.bobnet.android.rl.core.ents.Entity#tick()
+	 */
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see name.bobnet.android.rl.core.ents.Entity#processMessage(name.bobnet.android.rl.core.message.Message)
+	 */
+	@Override
+	public void processMessage(Message message) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
