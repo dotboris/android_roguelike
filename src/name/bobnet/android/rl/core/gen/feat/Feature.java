8799130f7ab0be7ac1844f86c689e87f726c3bbb
Boris Bera <bboris@rsoft.ca>
2011-10-22 16:22:21 -0400

    Added empty feature class

---
+++ src/name/bobnet/android/rl/core/gen/feat/Feature.java
@@ -0,0 +1,10 @@
+/*
+ * Class Name:			Feature.java
+ * Class Purpose:		A dungeon feature
+ * Created by:			boris on 2011-10-21
+ */
+package name.bobnet.android.rl.core.gen.feat;
+
+public abstract class Feature {
+
+}
