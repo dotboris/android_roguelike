6037351f9d82a47736539c085e93eb41325d0ad9
Boris Bera <bboris@rsoft.ca>
2011-10-23 13:03:50 -0400

    Completed the empty Feature class

--- src/name/bobnet/android/rl/core/gen/feat/Feature.java
+++ src/name/bobnet/android/rl/core/gen/feat/Feature.java
@@ -5,6 +5,56 @@
  */
 package name.bobnet.android.rl.core.gen.feat;
 
+import name.bobnet.android.rl.core.ents.Dungeon;
+
 public abstract class Feature {
+
+	// variables
+	protected Dungeon d;
+
+	/**
+	 * The possible sides of a feature
+	 * 
+	 * @author boris
+	 */
+	public enum Side {
+		NORTH, SOUTH, EAST, WEST
+	}
+
+	/**
+	 * Create the feature with a dungeon to work with
+	 * 
+	 * @param d
+	 *            the dungeon to work with
+	 */
+	public Feature(Dungeon d) {
+		this.d = d;
+	}
+
+	/**
+	 * Generate the random feature with random dimensions
+	 * 
+	 * The generator doesn't whether or not the feature will fit in the dungeon
+	 * 
+	 * @param x
+	 *            the x coordinate of the room entrance
+	 * @param y
+	 *            the y coordinate of the room entrance
+	 * @param s
+	 *            what side of the room the entrance is on
+	 */
+	public abstract void generate(int x, int y, Side s);
+
+	/**
+	 * Determine if the room can be placed inside the dungeon
+	 * 
+	 * @return true if the feature can be placed false otherwise
+	 */
+	public abstract boolean check();
+
+	/**
+	 * Place the feature in the dungeon
+	 */
+	public abstract void place();
 
 }
