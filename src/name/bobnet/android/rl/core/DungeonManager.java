40e201ad37b7c3663c3695c8222fa8155bf26725
Boris Bera <bboris@rsoft.ca>
2011-10-28 11:41:24 -0400

    Added empty Dungeon manager class

---
+++ src/name/bobnet/android/rl/core/DungeonManager.java
@@ -0,0 +1,25 @@
+/*
+ * Class Name:			DungeonManager.java
+ * Class Purpose:		Manages all the dungeons
+ * Created by:			boris on 2011-10-28
+ */
+package name.bobnet.android.rl.core;
+
+/**
+ * Manages all the dungeon.
+ * 
+ * <p>
+ * This class will:
+ * <ul>
+ * <li>Determine if a dungeon should be generated or loaded from file</li>
+ * <li>Generate Dungeons</li>
+ * <li>Save dungeons to file</li>
+ * <li>Load dungeons from file</li>
+ * </ul>
+ * </p>
+ * 
+ * @author boris
+ */
+public class DungeonManager {
+
+}
